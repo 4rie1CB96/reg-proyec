a7dbc7b01093fbb122b21f6501c0dc8d91c378e0
carlitos99 <barieldelvalle@gmail.com>
2025-08-15 13:03:22 -0300

    Reslpado 15-08-2025

---
+++ mi-proyecto/script.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+
+  loop: true,
+  autoplay: {
+    pauseOnMouseEnter: true,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  }
+});
